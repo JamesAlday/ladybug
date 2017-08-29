9cb845fe9f73fbaf6143e938c4775b290e6286da
JamesAlday <james@confiant.com>
2017-08-29 14:10:51 -0400

    Updated index page, added book and about pages

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 by Jeff Moeller in [The Things We Leave Behind](http://www.drivethrurpg.com/product/191250/The-Things-We-Leave-Behind)
 
-Handouts for Ladybug, Ladybug, Fly Away Home  in The Things We Leave Behind
+The homepage contains a countdown clock that uses [Flipclock](http://flipclockjs.com/). The code to initialize it is in [index page](index.html). I have it defaulted to 8pm 5 days from now. If you add an integer as a hash after the page, it will change the number of days. For example:
+
+```
+http://.../ladybug  --> 5 days
+http://.../ladybug#2 --> 2 days
+```
+
+This way the GM can customize how much time remains on the countdown depending on when the PCs first visit the website.
--- js/app.js
+++ js/app.js
@@ -8,28 +8,6 @@
 			$(".mobile-navigation").slideToggle();
 		});
 
-    	$(document).snow({ SnowImage: "images/floatingfrog.gif", Quantity: 20 });
-
-    	// Countdown Timer
-    	var to = new Date();
-    	var daysToAdd = 5;
-
-		if (window.location.hash) {
-			daysToAdd = parseInt(window.location.hash.split('#')[1].split(':'));
-		}
-
-    	var from = new Date();
-    	from.setDate(to.getDate() + daysToAdd);
-    	from.setHours(20);
-    	from.setMinutes(0);
-
-    	var dif = Math.floor((from.getTime() - to.getTime())/1000);
-
-		var clock = $('.countdown').FlipClock(dif,{
-			clockFace: 'DailyCounter',
-			countdown: true,
-		});
-
 	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
